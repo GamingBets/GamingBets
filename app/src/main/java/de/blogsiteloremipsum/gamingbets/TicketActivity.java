7951c6fc11eb80445552a404a1f9f990a06fb40f
Vegiepunk <niclas_petersohn@web.de>
2015-11-11 13:15:26 +0100

    - Created provisional UI - toolbar + menubar - Sign in Activity with E-Mail validation (no function) - ticket Activity (no function)

---
+++ app/src/main/java/de/blogsiteloremipsum/gamingbets/TicketActivity.java
@@ -0,0 +1,13 @@
+package de.blogsiteloremipsum.gamingbets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket);
+    }
+}

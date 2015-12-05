6593f4425fd8301d01e38953c01bd3b16236e87c
Vegiepunk <niclas_petersohn@web.de>
2015-12-05 15:12:58 +0100

    new UI Activitys

---
+++ app/src/main/java/de/blogsiteloremipsum/gamingbets/activity/EditUserActivity.java
@@ -0,0 +1,13 @@
+package de.blogsiteloremipsum.gamingbets.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+}

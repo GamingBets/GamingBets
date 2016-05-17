eb218ad1f8d820240997af9f1c291cbb139d84b3
Kapasko <felixmorsbach@web.de>
2016-05-18 00:01:40 +0200

    Implemented Show Profile

---
+++ app/src/main/java/de/blogsiteloremipsum/gamingbets/activity/ProfileActivity.java
@@ -0,0 +1,30 @@
+package de.blogsiteloremipsum.gamingbets.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import de.blogsiteloremipsum.gamingbets.R;
+import de.blogsiteloremipsum.gamingbets.classes.Globals;
+import de.blogsiteloremipsum.gamingbets.classes.User;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Globals g = (Globals) getApplication();
+        User u = g.getUser();
+
+
+        TextView username = (TextView) findViewById(R.id.txt_userName);
+        username.setText(u.getUserName());
+
+        TextView score = (TextView) findViewById(R.id.txt_score_value);
+        score.setText("60");
+        //score.setText(u.getScore());
+
+
+    }
+}
